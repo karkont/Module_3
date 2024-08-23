a042eca60ff62547b2b62d988334b92cec8f5219
Konstantin <kakoser91@gmail.com>
2024-08-23 18:13:49 +0300

    Add files via upload

---
+++ module_3_hard.py
@@ -0,0 +1,34 @@
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+
+def calculate_structure_sum(data_structure):
+    sum_numbers = 0
+    sum_strings = 0
+
+    def recurse(data):
+        nonlocal sum_numbers, sum_strings
+        if isinstance(data, list) or isinstance(data, tuple) or isinstance(data, set):
+            for i in data:
+                recurse(i)
+        elif isinstance(data, dict):
+            for value in data.values():
+                recurse(value)
+            for key in data.keys():
+                recurse(key)
+        elif isinstance(data, int) or isinstance(data, str):
+            if isinstance(data, int):
+                sum_numbers += data
+            elif isinstance(data, str):
+                sum_strings += len(data)
+
+    recurse(data_structure)
+    return sum_numbers + sum_strings
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
